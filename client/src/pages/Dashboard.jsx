4c90cf37a462ce989121ee41df5fb9428a5d94d1
ayushisahoo22 <ayushiis0912@gmail.com>
2026-06-28 20:40:32 +0530

    Add Chat and Panel components

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,10 +1,11 @@
 import Sidebar from "../components/Sidebar"
-
+import Chat from "../components/Chat"
 function Dashboard(){
     return(
-        <>
+        <div className="flex h-screen">
             <Sidebar/>    
-        </>
+            <Chat/>
+        </div>
     )
 }
 
